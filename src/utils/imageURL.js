0276a19cc05585629a0fe4087b0c6c68df8bb676
Triumphant <chanifan08@gmail.com>
2023-12-11 23:05:15 +0700

    url

--- src/utils/imageURL.js
+++ src/utils/imageURL.js
@@ -1,4 +1,5 @@
-const imgUrl = 'https://23.150.24.250:50183/downloadFile/';
+// const imgUrl = 'https://23.150.24.250:50183/downloadFile/';
 // const imgUrl = 'http://localhost:8080/downloadFile/';
+const imgUrl = 'http://192.168.100.12:8080/downloadFile/';
 
 export default imgUrl;
