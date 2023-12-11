3c5028c9df59dfd97940e9ffe58597680407683b
Triumphant <chanifan08@gmail.com>
2023-12-11 23:00:38 +0700

    Update imageURL.js

--- src/utils/imageURL.js
+++ src/utils/imageURL.js
@@ -1,4 +1,4 @@
-// const imgUrl = 'https://23.150.24.250:50183/downloadFile/';
-const imgUrl = 'http://localhost:8080/downloadFile/';
+const imgUrl = 'https://23.150.24.250:50183/downloadFile/';
+// const imgUrl = 'http://localhost:8080/downloadFile/';
 
 export default imgUrl;
